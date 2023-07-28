df1cdc746abef55b978cff01c7ae45e6dae6f884
David Poole <14233215+davidlpoole@users.noreply.github.com>
2023-07-28 12:07:39 +0100

    add a basic contract

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -7,22 +7,13 @@
 const hre = require("hardhat");
 
 async function main() {
-  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  const unlockTime = currentTimestampInSeconds + 60;
 
-  const lockedAmount = hre.ethers.parseEther("0.001");
+  const contract = await hre.ethers.deployContract("Contract");
 
-  const lock = await hre.ethers.deployContract("Lock", [unlockTime], {
-    value: lockedAmount,
-  });
+  await contract.waitForDeployment();
 
-  await lock.waitForDeployment();
-
-  console.log(
-    `Lock with ${ethers.formatEther(
-      lockedAmount
-    )}ETH and unlock timestamp ${unlockTime} deployed to ${lock.target}`
-  );
+  console.log(`Contract deployed to ${contract.target}`);
+  console.log(`Contract:`, contract);
 }
 
 // We recommend this pattern to be able to use async/await everywhere
